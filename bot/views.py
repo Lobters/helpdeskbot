ad43498d7d37536494e4996115917934a28e43f0
Artefakt-ff <fasqwerty123@mail.ru>
2018-12-22 00:09:28 +0300

    check webhook 2

--- bot/views.py
+++ bot/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 def index(request):
     with open('requests.txt', 'a') as f:
-        f.write(request.body)
+        f.write(str(request.body))
     return HttpResponse(True)

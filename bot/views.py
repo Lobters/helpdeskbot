6302a04d9759326d5a1c6dcf3b771b1f204104dd
Lobters <windbream@yandex.ru>
2018-12-22 22:47:37 +0300

    check json

--- bot/views.py
+++ bot/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+import json
 
 
 @csrf_exempt
 def index(request):
-    with open('requests.txt', 'a') as f:
-        f.write(str(request.body) + '')
+    jsonData = json.loads(str(request.body.decode('UTF-8')))
+    with open('test_requests.txt', 'a') as f:
+        print(type(jsonData))
     return HttpResponse(status=200)

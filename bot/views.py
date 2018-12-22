9128102a701a9e9f4d61d5a2f87cf25e5b88b6e0
Artefakt-ff <fasqwerty123@mail.ru>
2018-12-22 20:11:16 +0300

    add ignoring https refer

--- bot/views.py
+++ bot/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def index(request):
     with open('requests.txt', 'a') as f:
         f.write(str(request.body))

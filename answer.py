d8c31b38e474d5836956f80eb9fc92570cac723f
Lobters <windbream@yandex.ru>
2019-01-19 21:31:13 +0300

    check json

--- answer.py
+++ answer.py
@@ -6,10 +6,10 @@ def make_answer(raw_data):
     update_id = json_data['update_id']
     message = json_data['message']
     chat_id = message['chat']['id']
-    message_user = message['first_name']
-    message_text = message['text']
+    # message_user = message['first_name']
+    # message_text = message['text']
     base_url = 'https://api.telegram.org/'
     token = '795602800:AAElqQv6FqbcvoidTryBEvFL7-G4aI2KLts'
     method = 'sendMessage'
-    get_request = base_url + token + '/' + method + '?' + 'chat_id=' + chat_id + '&text=' + 'Hello'
+    get_request = base_url + token + '/' + method + '?' + 'chat_id=' + str(chat_id) + '&text=' + 'Hello'
     return get_request

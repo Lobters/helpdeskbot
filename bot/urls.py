fc6f5a25fc907f306a9b979e20c8af0987097948
a.bakulev <i@artbakulev.com>
2019-04-07 02:05:02 +0300

    Init parse message

--- bot/urls.py
+++ bot/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'722520790:AAEM0nUuaAD9BWFp0jv58VkeX3m-85DQOq', views.index, name='index'),
+    path(r'722520790:AAEM0nUuaAD9BWFp0jv58VkeX3m-85DQOq0', views.index, name='index'),
 ]
--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -7,12 +9,50 @@ import json
 
 from answer import make_answer
 
-
 logging.basicConfig(filename='requests.log', level=logging.INFO)
 
 
 @csrf_exempt
 def index(request):
-    raw = request.body.decode('utf-8')
-    logging.info(raw)
+    raw_message = request.body.decode('cp1251')
+    message = Message(raw_message)
+    logging.info(message)
     return HttpResponse(status=200)
+
+
+class Message:
+    def __init__(self, raw_message):
+        deserialized_message = json.loads(raw_message['message'])
+        self.message_id = deserialized_message['message_id']
+        self.user = User(deserialized_message['from'])
+        self.chat = Chat(deserialized_message['chat'])
+        self.date = datetime.datetime.fromtimestamp(deserialized_message['date'])
+        self.text = '123'
+
+    def __str__(self):
+        return '"{}" message from {}'.format(self.text, self.user)
+
+
+class User:
+    def __init__(self, user_object):
+        self.id = user_object.get('id')
+        self.is_bot = user_object.get('is_bot')
+        self.first_name = user_object.get('first_name')
+        self.last_name = user_object.get('last_name')
+        self.username = user_object.get('username')
+        self.language_code = user_object.get('language_code')
+
+    def __str__(self):
+        return self.username
+
+
+class Chat:
+    def __init__(self, chat_object):
+        self.id = chat_object.get('id')
+        self.first_name = chat_object.get('first_name')
+        self.last_name = chat_object.get('last_name')
+        self.username = chat_object.get('username')
+        self.type = chat_object.get('type')
+
+    def __str__(self):
+        return self.id

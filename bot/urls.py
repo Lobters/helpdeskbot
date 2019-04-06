1e23b5063350888fd19999c4950cfed268f7784d
a.bakulev <i@artbakulev.com>
2019-04-07 01:43:49 +0300

    Change token for tests

--- bot/urls.py
+++ bot/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'671563333:AAHtD7lA4gwITy-tYm6iSMnbHFE8gK9dtFc', views.index, name='index'),
+    path(r'722520790:AAEM0nUuaAD9BWFp0jv58VkeX3m-85DQOq', views.index, name='index'),
 ]

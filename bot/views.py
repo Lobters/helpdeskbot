922cfa9467bb3d09e78f71a879522298f36774df
a.bakulev <i@artbakulev.com>
2019-06-01 20:36:31 +0300

    Fix bugs

--- bot/views.py
+++ bot/views.py
@@ -14,11 +14,12 @@ logging.basicConfig(filename='views.log', level=logging.INFO)
 
 @csrf_exempt
 def index(request):
-    return HttpResponse(status=200)
 
     _, _, message = Postman.process_raw_request(request)
     postman = Postman(message)
     postman.send_response()
+    return HttpResponse(status=200)
+
 
 
 
